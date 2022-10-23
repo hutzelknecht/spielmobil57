349cc4dad48cc03cb051c7eeb1bc02f7a4021e7c
Michael Sinner <michael.sinner@adesso.de>
2022-10-23 17:11:32 +0200

    added @angular/fire

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spielmobil Karlsfeld
--- apps/spielmobil57/src/app/app.module.ts
+++ apps/spielmobil57/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp({
+      // TODO
+    })),
+    provideFirestore(() => getFirestore())
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule {}
